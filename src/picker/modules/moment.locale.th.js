e8b328dc8ba5d0399cd814e2aac1fae19a8c0ab0
Rax <rax@this.fish>
2022-07-27 18:53:21 +0800

    Added partial Thai Language, need confirmation from MK

---
+++ src/picker/modules/moment.locale.th.js
@@ -0,0 +1,64 @@
+//! moment.js locale configuration
+
+export default {
+  months: 'มกราคม_กุมภาพันธ์_มีนาคม_เมษายน_พฤษภาคม_มิถุนายน_กรกฎาคม_สิงหาคม_กันยายน_ตุลาคม_พฤศจิกายน_ธันวาคม'.split(
+    '_'
+  ),
+  monthsShort: 'มกรา_กุมภา_มีนา_เมษา_พฤษภา_มิถุนา_กรกฎา_สิงหา_กันยา_ตุลา_พฤศจิกา_ธันวา'.split('_'),
+  weekdays: 'อาทิตย์_จันทร์_อังคาร_พุธ_พฤหัสบดี_ศุกร์_เสาร์'.split(
+    '_'
+  ),
+  weekdaysShort: 'อาทิตย์_จันทร์_อังคาร_พุธ_พฤหัสบดี_ศุกร์_เสาร์'.split('_'),
+  weekdaysMin: 'อาทิตย์_จันทร์_อังคาร_พุธ_พฤหัสบดี_ศุกร์_เสาร์'.split('_'),
+  longDateFormat: {
+    LT: 'HH:mm',
+    LTS: 'HH:mm:ss',
+    L: 'DD/MM/YYYY',
+    LL: 'D MMMM YYYY',
+    LLL: 'D MMMM YYYY HH:mm',
+    LLLL: 'dddd, D MMMM YYYY HH:mm'
+  },
+  calendar: {
+    sameDay: '[Today at] LT',
+    nextDay: '[Tomorrow at] LT',
+    nextWeek: 'dddd [at] LT',
+    lastDay: '[Yesterday at] LT',
+    lastWeek: '[Last] dddd [at] LT',
+    sameElse: 'L'
+  },
+  relativeTime: {
+    future: 'in %s',
+    past: '%s ago',
+    s: 'a few seconds',
+    ss: '%d seconds',
+    m: 'a minute',
+    mm: '%d minutes',
+    h: 'an hour',
+    hh: '%d hours',
+    d: 'a day',
+    dd: '%d days',
+    M: 'a month',
+    MM: '%d months',
+    y: 'a year',
+    yy: '%d years'
+  },
+  dayOfMonthOrdinalParse: /\d{1,2}(st|nd|rd|th)/,
+  ordinal: function(number) {
+    var b = number % 10,
+      output =
+        ~~((number % 100) / 10) === 1
+          ? 'th'
+          : b === 1
+          ? 'st'
+          : b === 2
+          ? 'nd'
+          : b === 3
+          ? 'rd'
+          : 'th'
+    return number + output
+  },
+  week: {
+    dow: 1, // Monday is the first day of the week.
+    doy: 4 // The week that contains Jan 4th is the first week of the year.
+  }
+}
