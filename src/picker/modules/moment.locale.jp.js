2f86924ed7e427500e35c34169bf49ac70469c2e
Rax Otero <oterorax@gmail.com>
2025-05-23 19:57:52 +0800

    updated locales

--- src/picker/modules/moment.locale.jp.js
+++ src/picker/modules/moment.locale.jp.js
@@ -43,7 +43,7 @@ export default {
     return number + '日';
   },
   week: {
-    dow: 1, // Monday is the first day of the week.
-    doy: 4 // The week that contains Jan 4th is the first week of the year.
+    dow: 1, // Monday is the first day of the week
+    doy: 4  // The week that contains Jan 4th is the first week of the year
   }
 } 
